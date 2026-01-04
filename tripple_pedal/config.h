0cb312d2ff853c8bea1de24da2249a8259c8cc2f
Anthony <72553201+backAir@users.noreply.github.com>
2026-01-05 00:59:33 +0100

    trippel pedal

---
+++ tripple_pedal/config.h
@@ -0,0 +1,22 @@
+// Copyright 2024 Anthony (@bair)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+#define VIAL_KEYBOARD_UID {0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0x0D}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+
+
+
+
+
+#define FLASH_SIZE 0x1000000
+
+#define VIAL_MACRO_COUNT 64
