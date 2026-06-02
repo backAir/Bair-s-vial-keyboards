b5c7d2e3b8da057ebd75bb3394bad526b6baf7ef
Bair <no@mail.com>
2026-06-02 22:51:27 +0200

    hid mecropad2040

--- mecropad2040/keymaps/vial/keymap.c
+++ mecropad2040/keymaps/vial/keymap.c
@@ -3,6 +3,7 @@
 
 #include QMK_KEYBOARD_H
 #include "keymap_definitions.h"
+#include "raw_hid.h"
 
 enum custom_keycodes {
     KC_P00 = SAFE_RANGE
@@ -74,14 +75,38 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 #endif
 
 
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//     if (record->event.pressed) {
-//         switch(keycode) {
-//             case KC_P00:
-//                 tap_code(KC_P0);
-//                 tap_code(KC_P0);
-//                 return false;
-//         }
-//     }
-//     return true;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint8_t reply[32] = {0,1,2,3,30,19};
+    raw_hid_send(reply, 32);
+
+    // if(record->event.pressed && keycode == 0x8999){
+    //     led_state = !led_state;
+    //     writePin(LED_PIN, led_state);
+    //     return false;
+    // }
+
+    #include "../../../lib/process_record_user_HID.c"
+
+
+    return true;
+}
+
+// void keyboard_post_init_user(void) {
+//     setPinOutput(LED_PIN);
+//     writePinHigh(LED_PIN);
 // }
+
+void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
+// void raw_hid_receive(uint8_t *data, uint8_t length) {
+    // Your Raw HID handler here
+    // data[0..length-1]
+    // uint8_t reply[32] = {0};
+    led_state = !led_state;
+    writePin(LED_PIN, led_state);
+return;
+
+
+    #include "../../../lib/HID_layer_change.c"
+
+}
